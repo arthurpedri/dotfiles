ac0744800d82ab1ce915831eb7bc15d8ca3bd032
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2025-01-01 06:40:19 -0300

    Failed to integrate codeium with nvim-cmp and autocompletion

---
+++ nvim/.config/nvim/lua/custom/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  -- 'hrsh7th/nvim-cmp',
+  -- config = function()
+  --   local cmp = require 'cmp'
+  --   cmp.setup {
+  --     sources = cmp.config.sources {
+  --       { name = 'codeium' },
+  --       -- Keep the existing sources
+  --     },
+  --   }
+  -- end,
+}
