d7e44deca2a3e9ddc4caa350f141ffa8ad6146fa
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2026-06-05 22:30:14 -0300

    adds E B $ 0 switch and jk as esc

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+vim.keymap.set({ "n", "x", "o" }, "E", "$", { desc = "End of line" })
+vim.keymap.set({ "n", "x", "o" }, "$", "E", { desc = "End of WORD" })
+vim.keymap.set({ "n", "x", "o" }, "B", "0", { desc = "Start of line" })
+vim.keymap.set({ "n", "x", "o" }, "0", "B", { desc = "Start of WORD" })
